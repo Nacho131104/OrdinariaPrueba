a7e1f5d108d70cb3403ce690ff6baaaaefd0a1cb
Nacho131104 <nacholopezamat@gmail.com>
2025-01-28 17:02:28 +0100

    update

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 import { ApolloServer } from "@apollo/server";
 import { schema } from "./schema.ts";
-import { MongoClient } from "mongodb";
-import { DinosaurModel } from "./types.ts";
+//import { MongoClient } from "mongodb";
+import {  } from "./types.ts";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers } from "./resolvers.ts";
 
+/*
 const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
@@ -16,16 +17,16 @@ await mongoClient.connect();
 
 console.info("Connected to MongoDB");
 
-const mongoDB = mongoClient.db("dinosaurs");
-const DinosaursCollection = mongoDB.collection<DinosaurModel>("dinosaurs");
-
+const mongoDB = mongoClient.db("Personas");
+const DinosaursCollection = mongoDB.collection("Persona");
+*/
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
-  context: async () => ({ DinosaursCollection }),
+  context: async () => ({  }),
 });
 
 console.info(`Server ready at ${url}`);
